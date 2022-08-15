85c732a2e7b2501133c3c3b827860e0127b26669
Veronica Manuel <101472818+graceful-coder@users.noreply.github.com>
2022-08-15 11:10:19 -0400

    Test Script

---
+++ myepicproject.js
@@ -0,0 +1,73 @@
+const anchor = require("@project-serum/anchor");
+const { program } = require("@project-serum/anchor/dist/cjs/spl/associated-token");
+
+const { SystemProgram } = anchor.web3;
+
+const main = async() => {
+  console.log("🚀 Starting test...")
+
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  //anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Myepicproject;
+
+  //Create an account keypair for program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+  
+  //Call start_stuff_off, pass it the params it needs!
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
+  
+  console.log("📝 Your transaction signature", tx);
+
+  //Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
+
+  await program.rpc.addGif("insert_a_giphy_link_here", {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    },
+  });
+  
+  // Get the account again to see what changed.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
+
+  // Access gif_list on the account!
+  console.log('👀 GIF List', account.gifList)
+}
+
+
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
+
+// describe("myepicproject", () => {
+//   // Configure the client to use the local cluster.
+//   anchor.setProvider(anchor.AnchorProvider.env());
+
+//   it("Is initialized!", async () => {
+//     // Add your test here.
+//     const program = anchor.workspace.Myepicproject;
+//     const tx = await program.methods.initialize().rpc();
+//     console.log("Your transaction signature", tx);
+//   });
+// });
